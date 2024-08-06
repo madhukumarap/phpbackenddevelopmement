9f2a6fef685ab0a139f8cf74bc8a6078f3c0aba7
madhukumarap <madhukumarap07@gmail.com>
2024-08-06 14:58:54 +0530

    autoloading and psr

--- namespace/index.php
+++ namespace/index.php
@@ -1,8 +1,12 @@
 <?php 
 // They allow for better organization by grouping classes that work together to perform a task
 // They allow the same name to be used for more than one class
-require_once('Transaction.php');
+// require_once('Transaction.php');
 // var_dump(new madhu\Transaction()); or another type is there 
+
+spl_autoload_register(function($class){
+    var_dump($class);
+});
 
 use madhu\Transaction;
 var_dump(new Transaction());
