ec8df9f16d777056863be096d07f5a5ec032ce09
madhukumarap <madhukumarap07@gmail.com>
2024-08-11 23:25:13 +0530

    updates

---
+++ test/unit/RouterTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace test\unit;
+use phpDocumentor\Reflection\Types\Void_;
+use PHPUnit\Framework\TestCase;
+class RouterTest extends TestCase{
+    public function it_register_a_route(): Void
+    {
+        
+    }   
+}
